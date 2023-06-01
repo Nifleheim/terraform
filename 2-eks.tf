3937d55a99e0aa60fcafa4b42b5894160e4d3fc4
Reza Fabryan <123403871+Nifleheim@users.noreply.github.com>
2023-06-02 02:49:05 +0700

    Update 2-eks.tf

--- 2-eks.tf
+++ 2-eks.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~>4.62"
-    }
-  }
-}
-
 resource "aws_iam_role" "eks" {
     name = "${var.env}-${var.eks_name}-eks-cluster"
     
